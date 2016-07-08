fdba3db3ee9626fd1d061f004035d152604cfda2
adamsyntel <Adam_Neuses@syntelinc.com>
2016-07-08 09:15:58 -0700

    Add files via upload

---
+++ string_concat.java
@@ -0,0 +1,17 @@
+class string_concat{
+	
+	public static void main(String[] args){
+
+		/* String.concat will add one string to the end of another string
+		 */
+		String code = "123";
+		String code2 = "7704";
+
+		String fullcode = code.concat(code2);
+		
+		System.out.println("Full string is: " + fullcode);
+		
+	}
+
+
+}
